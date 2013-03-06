d510ebacde29b4f169322e2d02e166a50ea5ec1c
DeliriumCordia <fabrizio.onori@yahoo.it>
2013-03-06 17:03:46 +0100

    metodo nuovo

--- 20130306/esercizio01.js
+++ 20130306/esercizio01.js
@@ -2,10 +2,10 @@ var i;
 var string1;
 for(i=1;i<101;i++){
 	if((i%10)===0){
-		i.concat(string1);
+		string1=i.concat(string1);
 		console.log(string1);
 		string1="";
 	}
-	else i.concat(string1);
+	else string1=i.concat(string1);
 }
 	
