143412253acda985584578e3b779042e543aab62
DeliriumCordia <fabrizio.onori@yahoo.it>
2013-03-06 17:31:00 +0100

    v1.31

--- 20130306/esercizio02.js
+++ 20130306/esercizio02.js
@@ -6,7 +6,7 @@ string1="";
 for(i=1;i<11;i++){
 for(z=1;z<11;z++){
 string1=string1.concat(i*z);
-if((i*z)%10!==0){
+if(((i*z)%10)!==0){
 if((i*z)<10)
 string1=string1.concat(", ");
 else 
@@ -15,3 +15,4 @@ string1=string1.concat(",");
 console.log(string1);
 string1="";
 }	
+}
