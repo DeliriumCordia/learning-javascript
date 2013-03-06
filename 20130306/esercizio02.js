ac904978408ab8049097c0793aa9e0ea44bfda44
DeliriumCordia <fabrizio.onori@yahoo.it>
2013-03-06 17:29:03 +0100

    v1.3

---
+++ 20130306/esercizio02.js
@@ -0,0 +1,17 @@
+var i;
+var z;
+var string1;
+string1="";
+
+for(i=1;i<11;i++){
+for(z=1;z<11;z++){
+string1=string1.concat(i*z);
+if((i*z)%10!==0){
+if((i*z)<10)
+string1=string1.concat(", ");
+else 
+string1=string1.concat(",");
+}
+console.log(string1);
+string1="";
+}	
