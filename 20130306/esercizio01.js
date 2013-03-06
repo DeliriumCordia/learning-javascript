4a9b3cf3263ce05c425f887365b9acc21b7175e9
DeliriumCordia <fabrizio.onori@yahoo.it>
2013-03-06 17:23:27 +0100

    v1.3

--- 20130306/esercizio01.js
+++ 20130306/esercizio01.js
@@ -6,6 +6,9 @@ string1="";
 for(i=1;i<11;i++){
 for(z=1;z<11;z++){
 string1=string1.concat(i*z);
+if((i*z)<10)
+string1=string1.concat("  ");
+else 
 string1=string1.concat(" ");
 }
 console.log(string1);
