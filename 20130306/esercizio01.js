42f30e98c59eec600458cc6c6a66335042eba1cf
DeliriumCordia <fabrizio.onori@yahoo.it>
2013-03-07 11:50:12 +0100

    tab

--- 20130306/esercizio01.js
+++ 20130306/esercizio01.js
@@ -6,10 +6,7 @@ string1="";
 for(i=1;i<11;i++){
 for(z=1;z<11;z++){
 string1=string1.concat(i*z);
-if((i*z)<10)
-string1=string1.concat("  ");
-else 
-string1=string1.concat(" ");
+string1=string1.concat("\t");
 }
 console.log(string1);
 string1="";
